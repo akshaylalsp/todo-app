95375e442c6ea461ba92807f94390266e53f247f
akzaylal <akshaylalsp6@gmail.com>
2024-05-03 11:36:50 +0530

    created a custom user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,8 @@
+from typing import Any
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class CustomUser(AbstractUser):
+
+    address = models.CharField(max_length=100,null=False,blank=True)
+
