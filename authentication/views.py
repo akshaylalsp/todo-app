5fe9fd03f08fdde8cbdbd8e7841bc2129097076a
akzaylal <akshaylalsp6@gmail.com>
2024-05-03 11:11:23 +0530

    created sample view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def login_view(request):
+    return HttpResponse("Login Page")
+
+def logout_view(request):
+    return HttpResponse("Logout Page")
+
+def register_view(request):
+    return HttpResponse("Register Page")
