0340c6c44226ad837c27efeb9d9bd97bbc3d8463
akzaylal <akshaylalsp6@gmail.com>
2024-09-22 14:13:01 +0530

    added auth views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserRegistrationForm
 
 # Create your views here.
+
+def login_view(request):
+    pass 
+
+@login_required
+def logout_view(request):
+    pass
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form =  CustomUserRegistrationForm(request.POST)
+        # print(form)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('user created')
+        else:
+            print('invalid form',form.errors)
+    form = CustomUserRegistrationForm()
+        
+    return render(request,'register.html',{'form':form})
