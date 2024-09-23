d3714da78a91240810afb10a6725a51b5194f96f
akzaylal <akshaylalsp6@gmail.com>
2024-09-23 20:11:36 +0530

    login and task create form

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,10 +1,21 @@
 from .models import CustomUser,Tasks
-from django.forms import ModelForm
+from django.forms import ModelForm,Form
 from django.contrib.auth.forms import UserCreationForm
-
+from django import forms
 class CustomUserRegistrationForm(UserCreationForm):
     
     
     class Meta:
         model = CustomUser
         fields = ['username','phone']
+
+
+class TaskCreateForm(ModelForm):
+
+    class Meta:
+        model = Tasks
+        fields = ['name','description']
+
+class LoginForm(Form):
+    username = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
