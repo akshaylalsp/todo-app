dcf9dd794e9759a785b9d4b5fcf5f358f8ac6cbf
akzaylal <akshaylalsp6@gmail.com>
2024-09-24 20:04:16 +0530

    edit task

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserRegistrationForm,TaskCreateForm,LoginForm
+from .forms import CustomUserRegistrationForm,TaskCreateForm,LoginForm,TaskUpdateForm
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login,authenticate
+from .models import Tasks
 # Create your views here.
 
 def login_view(request):
@@ -27,6 +28,29 @@ def login_view(request):
 @login_required
 def logout_view(request):
     return redirect('login')
+
+@login_required
+def edit_task_view(request,id):
+    if request.method == 'POST':
+        form = TaskUpdateForm(request.POST)
+        if form.is_valid():
+            item = Tasks.objects.get(id=id)
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            is_completed = form.cleaned_data['is_completed']
+            if item:
+                item.name = name 
+                item.description = description
+                item.is_completed = is_completed
+                item.save()
+                return HttpResponse('updated')
+            else:
+                return HttpResponse('that task doesnt exists')
+        else:
+            return HttpResponse('invalid form')
+    else:
+        form = TaskUpdateForm()
+        return render(request,'add_task.html',{'form':form})
 
 
 
